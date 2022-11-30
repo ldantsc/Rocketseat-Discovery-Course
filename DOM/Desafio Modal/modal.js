3154f9a7d19ae256c721e0297394023d0df720ed
Lucas Dantas <lucasdantasguitar@gmail.com>
2022-11-30 18:33:12 -0300

    30/Nov

--- DOM/Desafio Modal/modal.js
+++ DOM/Desafio Modal/modal.js
@@ -1,11 +1,17 @@
 const button = document.getElementById("openModal")
+const div = document.querySelector('div')
 
-button.addEventListener('click', buttonClick)
-
-function buttonClick() {
-    const element = document.querySelector('div')
-    element.classList.remove('invisible')
+button.onclick = function() {
+    div.classList.remove('invisible')
     console.log("clicou")
 }
 
-    
+
+document.addEventListener('keydown', function(event) {
+    console.log(event)
+    const isEscKey = event.key === 'Escape'
+
+    if (isEscKey) {
+        div.classList.add('invisible')
+    }
+})
